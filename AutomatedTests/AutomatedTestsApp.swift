491e93cef28a29b687b90f5a47fa103adbd14aa4
heartsker <heartsker@gmail.com>
2021-09-19 13:05:23 +0300

    initial commit

---
+++ AutomatedTests/AutomatedTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomatedTestsApp.swift
+//  AutomatedTests
+//
+//  Created by Daniel Pustotin on 19.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct AutomatedTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
